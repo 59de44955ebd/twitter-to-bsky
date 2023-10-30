b7035fab35420d05780e595100433fb371708a21
59de44955ebd <59de44955ebd@gmail.com>
2023-10-30 11:31:22 +0100

    added @namespace

--- twitter-to-bsky.user.js
+++ twitter-to-bsky.user.js
@@ -3,6 +3,7 @@
 // @version        0.5
 // @description    Crosspost from Twitter to Bluesky
 // @author         59de44955ebd
+// @namespace      59de44955ebd
 // @license        MIT
 // @match          https://twitter.com/*
 // @icon           https://raw.githubusercontent.com/59de44955ebd/twitter-to-bsky/main/bsky-32x32.png
